249a1613c580818157a90e0ff99f57962e1eb993
proman655 <proman655@Knights.ucf.edu>
2021-11-01 19:35:32 -0400

    Fresh dependencies added

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.listen(5000, console.log('Server started on PORT 5000'));
+
+app.get("/", (req, res) => {
+    res.send("API is running..");
+});
