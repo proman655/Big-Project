8a032a8c9c72903d5ad5a19cb11ed5137a269ee2
phill-coder <phillip.ng.le@gmail.com>
2021-11-02 17:34:12 -0400

    connected mongoDB
    Did npm i mongoose.
    Added config folder with db.js that connects to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+connectDB();
 app.listen(5000, console.log('Server started on PORT 5000'));
 
 app.get("/", (req, res) => {
