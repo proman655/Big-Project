a0893b393c5d88481926e480ec953cef4d6f6c41
phill-coder <phillip.ng.le@gmail.com>
2021-11-02 21:04:23 -0400

    added user routes
    added userRoutes and for now users do localhost:5000/api/User to run api

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const UserRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
+app.use(express.json());
 app.listen(5000, console.log('Server started on PORT 5000'));
 
 app.get("/", (req, res) => {
     res.send("API is running..");
 });
+app.use('/api/User', UserRoutes);
