70eeefdf939897f4d7895f996d9d0ca6e31633f2
ChristianSilva1 <59715321+ChristianSilva1@users.noreply.github.com>
2021-11-08 16:18:06 -0500

    Enable Cors

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,18 @@ app.use(express.json());
 app.listen(port, () => console.log(`Server started on ${port}`));
 
 //enable cors
-app.get('/cors', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.send({ "msg": "This has CORS enabled 🎈" })
-    })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
 //base
 app.get("/", (req, res) => {
