367b636c2f00a85230ee0016033021fc499a5845
ChristianSilva1 <59715321+ChristianSilva1@users.noreply.github.com>
2021-11-07 22:44:10 -0500

    Registration Complete

---
+++ frontend/src/register/RegisterScreen.js
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import Logo from "../pictures/logo-register.png";
+import RegisterButton from "../pictures/register-button.png";
+import CancelButton from "../pictures/cancel-button.png";
+import { Link } from "react-router-dom";
+import "./register.css";
+import axios from "axios";
+
+const RegisterScreen = () => {
+  const [Email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+  const [phoneNumber, setPhone] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== passwordRepeat) {
+      console.log("Passwords do not match");
+    } else {
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+
+        const { data } = await axios.post(
+          "http://localhost:5000/api/user/register",
+          {
+            firstName,
+            lastName,
+            Email,
+            password,
+            phoneNumber,
+          },
+          config
+        );
+        console.log(data);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error) {
+        console.log("ERROR");
+      }
+    }
+    console.log(Email);
+  };
+
+  return (
+    <div className="div-register">
+      <div className="div-register-logo">
+        <img src={Logo} alt="logo" className="logo-img" />
+      </div>
+      <div className="div-register-form">
+        <form onSubmit={submitHandler}>
+          <div className="register-form-group">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              placeholder="First name"
+              className="register-input"
+              required
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+          </div>
+          <div className="register-form-group">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              placeholder="Last name"
+              className="register-input"
+              required
+              onChange={(e) => setLastName(e.target.value)}
+            ></input>
+          </div>
+          <div className="register-form-group">
+            <input
+              type="email"
+              name="email"
+              value={Email}
+              placeholder="Email"
+              className="register-input"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div className="register-form-group">
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              className="register-input"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <div className="register-form-group">
+            <input
+              type="password"
+              name="passwordRepeat"
+              value={passwordRepeat}
+              placeholder="Re-type Password"
+              className="register-input"
+              required
+              onChange={(e) => setPasswordRepeat(e.target.value)}
+            ></input>
+          </div>
+          <div className="register-form-group">
+            <input
+              type="phone"
+              name="phoneNumber"
+              value={phoneNumber}
+              placeholder="Phone Number"
+              className="register-input"
+              required
+              onChange={(e) => setPhone(e.target.value)}
+            ></input>
+          </div>
+          <div className="buttons-div">
+            <Link to="/">
+              <button renderAs="button" className="cancel-button">
+                <img
+                  src={CancelButton}
+                  alt="cancel"
+                  className="register-button"
+                />
+              </button>
+            </Link>
+            <button onSubmit={submitHandler} className="register-button">
+              <img
+                src={RegisterButton}
+                alt="register"
+                className="register-button"
+              />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterScreen;
