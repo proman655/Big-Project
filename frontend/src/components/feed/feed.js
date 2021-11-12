8bfdfea140bf194200fe1eac1a7a513360648f21
GusCornejo <gustavoxcornejo@knights.ucf.edu>
2021-11-12 18:54:23 -0500

    Add project form added

--- frontend/src/components/feed/feed.js
+++ frontend/src/components/feed/feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { NavLink, Link } from 'react-router-dom'
 import Placehholder from "../../pictures/card-placeholder.jpg"
+import CreateProjectButton from "../../pictures/create-project-button.png"
+import CancelButton from "../../pictures/cancel-button.png"
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
@@ -9,7 +14,63 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './feed.css'
 
 
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal className="modal"
+            {...props}
+            dialogClassName="modal-80w"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton className="modal-header">
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Create New Project
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="modal-body">
+                <Form className="new-project-form">
+                    <div className="new-project-form-group">
+                        <input type="text" name="Title" placeholder="Title" className="new-project-input"></input>
+                    </div>
+                    <div className="new-project-form-group">
+                        <input type="text" name="Project Manager" placeholder="Project manager" className="new-project-input"></input>
+                    </div>
+                    <div className="new-project-form-group">
+                        <input type="date" name="dueDate" className="new-project-input"></input>
+                    </div>
+                    <div className="new-project-form-group">
+                        <select aria-label="Default select example" className="new-project-input">
+                            <option>None</option>
+                            <option value="1">Tev</option>
+                            <option value="2">Chris</option>
+                            <option value="3">Gus</option>
+                            <option value="3">Pedro</option>
+                        </select>
+                    </div>
+                    <div className="new-project-form-group-box">
+                        <textarea type="text" name="Project Manager" placeholder="Description" className="new-project-input" id="textBox"></textarea>
+                    </div>
+               
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <button className="new-project-button">
+                    <img src={CreateProjectButton} alt="submit" />
+                </button>
+                <button onClick={props.onHide} className="new-project-button">
+                    <img src={CancelButton} alt="cancel"/>
+                </button>
+                    
+              
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
 const Feed = () => {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
 
         <Container className="feed-container">
@@ -43,9 +104,14 @@ const Feed = () => {
                 </Row>
             </div>
             
-            <div className="add-button-div">
+            <div className="add-button-div" onClick={() => setModalShow(true)}>
                 <button id="add-button" />
             </div>
+
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
 
         </Container>
 
