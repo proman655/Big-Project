8fde02756933ac3bc381bc431afa570be7aca078
proman655 <proman655@Knights.ucf.edu>
2021-11-16 17:19:17 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production')
 
   app.get('*', (req, res) => 
  {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'src', 'login', 'LoginScreen.js'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   });
 }
 
