a55f0ba2820d0a471b486dda5b24a6d777bebee6
proman655 <89658114+proman655@users.noreply.github.com>
2021-11-28 21:17:37 -0500

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,29 +3,13 @@ const User = require("../models/userModel");
 const generateToken = require("../utils/generateToken");
 const crypto = require('crypto');
 const sgMail = require('@sendgrid/mail');
+const router = require("../routes/userRoutes");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
   const userExists = await User.findOne({ email });
-
-  const msg = {
-    from: 'noreply@email.com',
-    to: user.email,
-    subject: 'Verify your Email',
-    test: `
-      Hello, thanks for registering on our site.
-      Please copy and paste the address below to verify your account.
-      http//${req.headers.host}/verify-email?token=${user.emailToken}
-    `,
-    html: `
-      <h1>Hello,</h1>
-      <p>Thanks for registering on our site.</p>
-      <Please click the link below to verify your account.</p>
-      <a href="http://${req.headers.host}/verify-email?token${user.emailToken}">Verify your account</a>
-    `
-  }
 
   if (userExists) {
     res.status(400);
@@ -47,12 +31,58 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       pic: user.pic,
       token: generateToken(user._id),
+      emailToken: crypto.randomBytes(64).toString('hex'),
+      isVerified: false,
     });
+
+    //const emailtoken = crypto.randomBytes(64).toString('hex');
+
+    const msg = {
+      from: 'noreply@email.com',
+      to: user.email,
+      subject: 'Verify your Email',
+      test: `
+        Hello, thanks for registering on our site.
+        Please copy and paste the address below to verify your account.
+        http//${req.headers.host}/verify-email?token=${user.emailToken}
+      `,
+      html: `
+        <h1>Hello,</h1>
+        <p>Thanks for registering on our site.</p>
+        <Please click the link below to verify your account.</p>
+        <a href="http://${req.headers.host}/verify-email?token${user.emailToken}">Verify your account</a>
+      `
+    }
+    try{
+      await sgMail.send(msg);
+      req.flash('Success', 'Thanks for registering. Please check your email to verify your account');
+      res.redirect('/');
+    } catch(error){
+      console.log(error);
+    }
+
   } else {
     res.status(400);
     throw new Error("User not found");
   }
 });
+
+// Email verification route
+router.get('/verify-email', async(req, res, next) => {
+  try{
+    const user = await User.findOne({ emailToken: req.query.token });
+    if (!user) {
+      req.flash('error', 'Token is invalid.');
+      return res.redirect('/');
+    }
+    user.emailToken = null;
+    user.isVerified = true;
+    await user.save();
+  } catch(error){
+    console.log(error);
+    res.redirect('/');
+  }
+})
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
